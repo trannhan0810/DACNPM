2a16daf6c3b4bf5a29dfeea18a63411e5ebd52c3
trannhan0810 <trannhan08101999@gmail.com>
2020-10-29 09:13:49 +0700

    Done api docs brand

--- src/loaders/swaggerDocs.js
+++ src/loaders/swaggerDocs.js
@@ -6,7 +6,7 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "LogRocket Express API with Swagger",
+            title: "Log E-Commerce app Express API with Swagger",
             version: "0.1.0",
             description: "This is a simple CRUD API application made with Express and documented with Swagger",
         },
