1b1e63baaf8a8db6dae687e02a7adc29f0c54312
trannhan0810 <trannhan08101999@gmail.com>
2020-12-05 10:11:52 +0700

    Update to express5

--- src/api/modules/user/controller.js
+++ src/api/modules/user/controller.js
@@ -1,9 +1,5 @@
-import { removeAllListeners } from 'nodemon';
-import User from '../../../database/schemas/User';
 import Controller from '../../core/Controller'
 import UserService from "./service"
-const Joi = require('joi')
-const jwt = require('jsonwebtoken')
 export default class UserController extends Controller{
 
     service = UserService.getService();
