ad1ad50486a444017747e44c7c02f5a2f600f3ad
Nguyễn Tiến Văn <vannt808@gmail.com>
2020-12-22 12:46:10 +0700

    full order for Admin/ Technical

--- src/api/modules/orders/routes.js
+++ src/api/modules/orders/routes.js
@@ -5,7 +5,7 @@ import OrderController from './controller'
 const routes = Router()
 const orderController = new OrderController()
 
-routes.get("/", orderController.getMany.bind(orderController))
+routes.get("/", orderController.getFullOrder.bind(orderController))
 routes.get("/:id", orderController.getById.bind(orderController))
 routes.post("/", orderController.createOne.bind(orderController))
 routes.delete("/:id", orderController.deleteOne.bind(orderController))
