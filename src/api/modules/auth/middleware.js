2cd1a65465f692a60514b2d494363cfd70ecdb61
trannhan0810 <trannhan08101999@gmail.com>
2020-12-26 10:02:01 +0700

    Improve author

--- src/api/modules/auth/middleware.js
+++ src/api/modules/auth/middleware.js
@@ -4,24 +4,43 @@ import AuthService from "./service"
 import UserRepository from './repository'
 
 export default async (req, res, next) => {
+
+    const authHeader = req.headers['authorization']
+    const token  =  authHeader && authHeader.split(' ')[1]
+    if(token == null) throw Boom.unauthorized()
+    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+        if(err) next(Boom.unauthorized("Wrong acess token"))
+        req.user = user;
+        const api_path = req.baseUrl + req.route.path
+        const api_method = req.method
+        const haveAuthor = await AuthService.getService().authorize(user.id, api_path, api_method)
+        if(haveAuthor == false) next(Boom.forbidden("Not have permission"))
+        next() 
+    })  
+
+
+
+    /*
     const authHeader = req.headers['authorization']
     
     const api_path = req.baseUrl + req.route.path
     const api_method = req.method
-    const userPermiss = await AuthService.getService().getUserPermission(user,id)
-    const routePermiss = await AuthService.getService().getRoutePermission(api_path, api_method)
-    if(routePermiss == null) next()
-    else {
-        const token  =  authHeader && authHeader.split(' ')[1]
-        if(token == null) throw Boom.unauthorized()
-
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
-            if(err) next(Boom.unauthorized("Wrong acess token"))
-            req.user = user;
     
-            
-            if(userPermission.find((value)=>((String)(value.id_per)==(String)(routePermission.id_per))))  next()  
-            else next(Boom.forbidden("Not have permission"))
-        })  
-    }
+    const routePermiss = await AuthService.getService().getRoutePermission(api_path, api_method)
+   
+    const token  =  authHeader && authHeader.split(' ')[1]
+    
+
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+        if(err) next(Boom.unauthorized("Wrong acess token"))
+        req.user = user;
+        console.log(user)
+        if(routePermiss == null) next()
+        if(token == null) throw Boom.unauthorized()
+        const userPermiss = await AuthService.getService().getUserPermission(user.id)
+        if(userPermiss.find((value)=>((String)(value.id_per)==(String)(routePermiss.id_per))))  next()  
+        else next(Boom.forbidden("Not have permission"))
+    })  //*/
+    
 }
